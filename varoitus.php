077e0dfc7d9faf6390ffb93502539fb68dd291c2
JohannnaM <johanna.mattsson@edu.omnia.fi>
2022-09-13 17:16:51 +0300

    muutos

--- varoitus.php
+++ varoitus.php
@@ -1,1 +1,24 @@
-$xml=simplexml_load_file("autot.xml") or die("Virhe: XML-syötteen käsittely epäonnistui");
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$xml=simplexml_load_file("alertsfeed.xml") or die("Virhe: XML-syötteen käsittely epäonnistui");
+
+foreach($xml->children() as $channel) {
+    echo $channel->title. "<br>";
+    echo $channel->description. "<br>";
+    echo $channel->pubDate. "<br>";
+    echo "<br>";
+
+    foreach($channel->children() as $item) {
+        echo $item->title. "<br>";
+        echo $item->description. "<br>";
+        echo $item->pubDate. "<br>";
+        echo "<br>";
+    }
+}
+?>
+
+</body>
+</html>
